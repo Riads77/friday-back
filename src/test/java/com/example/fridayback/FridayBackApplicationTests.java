db5233a32426e7e00b703a72066ba67b41f19a6b
Riads77 <adrienroumegous@hotmail.com>
2021-12-27 07:29:15 +0100

    Initial commit

---
+++ src/test/java/com/example/fridayback/FridayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fridayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
